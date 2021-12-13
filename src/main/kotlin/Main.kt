f131fbe6393acf54a609592d06b52415959247b8
LaoLittle <y1941803875@outlook.com>
2021-12-13 19:28:16 +0800

    添加判断

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 import Yinglish.chs2yin
 
 fun main(args: Array<String>) {
-    println(args[0].chs2yin(args[1].toInt()))
+    if (args[0].isEmpty()) {
+        println("没有内容！")
+        return
+    }
+    val yingLevel = if (args[1].matches(Regex("""\D""")) || args[1].isEmpty()) 50 else args[1].toInt()
+    println(args[0].chs2yin(yingLevel))
 }
 
